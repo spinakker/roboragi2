5801be75b4e742928641a48db79908439c6095bd
spinakker <friedl.mate@gmail.com>
2017-03-29 16:30:15 +0200

    recursive channel archiver method, but it's broken because of GetMessages()

--- roboragi2/BotProgram.cs
+++ roboragi2/BotProgram.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
@@ -68,6 +69,22 @@ namespace roboragi2
                 await e.Message.Respond ("pong");
             });
 
+            this.Commands.AddCommand ("savepics", async e => {
+                await ArchivePics (e.Channel);
+                await e.Message.Respond ("");
+            });
+
+
+            this.Client.GuildAvailable += e => {
+                this.Client.DebugLogger.LogMessage (LogLevel.Info, "discord bot", $"Guild available: {e.Guild.Name}", DateTime.Now);
+                return Task.Delay (0);
+            };
+
+            this.Client.MessageCreated += e => {
+                this.Client.DebugLogger.LogMessage (LogLevel.Info, "discord bot", $"Message recieved: {e.Message.Content}", DateTime.Now);
+                return Task.Delay (0);
+            };
+
             await this.Client.Connect ();
 
             await Task.Delay (-1);
@@ -75,6 +92,39 @@ namespace roboragi2
 
         private void DebugLogger_LogMessageReceived (object sender, DebugLogMessageEventArgs e) {
             Console.WriteLine ($"[{e.TimeStamp.ToString ("yyyy-MM-dd HH:mm:ss")}] [{e.Level}] {e.Message}");
+        }
+
+        private async Task ArchivePics (DiscordChannel channel, int archiveLimit = 0) {
+
+            List<string> picURLs = new List<string> ();
+
+            await GetPicAddressesFromChannel (channel, channel.LastMessageID, picURLs);
+
+            foreach (var picURL in picURLs) {
+                this.Client.DebugLogger.LogMessage (LogLevel.Info, "discord bot", $"pic urls found: {picURL}", DateTime.Now);
+            }
+
+            using (var client = new WebClient ())
+            {
+                client.DownloadFileAsync (new Uri (picURLs[0]), "testpic.jpeg");
+            }
+        }
+
+        private async Task GetPicAddressesFromChannel (DiscordChannel channel, ulong afterMessageId, List<string> picURLs) {
+
+            // non of the arguments get passed here waiting for fix, that's why it's broken
+            List<DiscordMessage> messages = await channel.GetMessages (after: afterMessageId, limit: 100);
+
+            foreach (var message in messages) {
+                this.Client.DebugLogger.LogMessage (LogLevel.Info, "discord bot", $"Message found: {message.Content}", DateTime.Now);
+                if (message.Content.StartsWith ("http"))
+                    picURLs.Add (message.Content);
+            }
+
+            if (messages.Count < 50)
+                return;
+            else
+                await GetPicAddressesFromChannel (channel, messages.Last ().ID, picURLs);
         }
     }
 }
