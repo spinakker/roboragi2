90a968c8ecc9f30b1dd56b953623f9c49fa332e6
spinakker <friedl.mate@gmail.com>
2017-03-29 17:42:43 +0200

    simple commands added

--- roboragi2/BotProgram.cs
+++ roboragi2/BotProgram.cs
@@ -5,6 +5,7 @@ using System.IO;
 using Newtonsoft.Json;
 
 using Discord;
+using Discord.Commands;
 
 
 namespace roboragi2
@@ -32,12 +33,44 @@ namespace roboragi2
             this.Configuration = config;
         }
 
+
         public void Start () {
             client = new DiscordClient();
 
             //stuff comes here
 
+            // logging
             client.Log.Message += (s, e) => Console.WriteLine ($"[{e.Severity}] {e.Source}: {e.Message}");
+
+
+            // commands
+            client.UsingCommands (x => {
+                x.PrefixChar = '!';
+                x.HelpMode = HelpMode.Public;
+            });
+
+            client.GetService<CommandService> ().CreateGroup ("do", cgb =>
+            {
+                cgb.CreateCommand ("greet")
+                        .Alias (new string[] { "gr", "hi" })
+                        .Description ("Greets a person.")
+                        .Parameter ("GreetedPerson", ParameterType.Required)
+                        .Do (async e =>
+                        {
+                            await e.Channel.SendMessage ($"{e.User.Name} greets {e.GetArg ("GreetedPerson")}");
+                        });
+
+                cgb.CreateCommand ("bye")
+                        .Alias (new string[] { "bb", "gb" })
+                        .Description ("Greets a person.")
+                        .Parameter ("GreetedPerson", ParameterType.Required)
+                        .Do (async e =>
+                        {
+                            await e.Channel.SendMessage ($"{e.User.Name} says goodbye to {e.GetArg ("GreetedPerson")}");
+                        });
+            });
+
+
 
             client.ExecuteAndWait (async () => {
                 await client.Connect (token: Configuration.Token, tokenType: TokenType.Bot);
