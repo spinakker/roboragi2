a42857cb9e5ea58dd588ab4f35f38f560b9aa6d7
spinakker <friedl.mate@gmail.com>
2017-08-05 00:37:34 +0200

    user archiver progress, needs solution for users that have left the server

--- roboragi2/Archiver.cs
+++ roboragi2/Archiver.cs
@@ -1,79 +1,69 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
-
 using Discord;
 
 
-namespace roboragi2
-{
-    internal class Archiver
-    {
-        public Archiver (ref DiscordClient clientPar)
-        {
+namespace roboragi2 {
+    internal class Archiver {
+        public Archiver(ref DiscordClient clientPar) {
             _client = clientPar;
         }
 
         private readonly DiscordClient _client;
 
-        public async Task ArchivePics (Channel channel, int archiveLimit = 0)
-        {
-
+        public async Task ArchivePics(Channel channel, int archiveLimit = 0) {
             List<string> picUrLs = new List<string>();
 
-            await GetPicAddressesFromChannel (channel, picUrLs, null, archiveLimit);
+            await GetPicAddressesFromChannel(channel, picUrLs, null, archiveLimit);
 
-            _client.Log.Info (this.ToString (), "Saving " + picUrLs.Count.ToString () + " pictures...");
+            _client.Log.Info(this.ToString(), "Saving " + picUrLs.Count.ToString() + " pictures...");
 
-            using (var webClient = new WebClient ())
-            {
+            using (var webClient = new WebClient()) {
                 string newFolderName = channel.Name;
-                if (!System.IO.Directory.Exists (newFolderName))
-                    System.IO.Directory.CreateDirectory (newFolderName);
+                if (!System.IO.Directory.Exists(newFolderName))
+                    System.IO.Directory.CreateDirectory(newFolderName);
 
-                foreach (var uriString in picUrLs)
-                {
+                foreach (var uriString in picUrLs) {
                     string filename = uriString.Split('/').Last();
 
-                    if (!string.IsNullOrEmpty (filename) && filename.Length > 20)
-                    {
+                    if (!string.IsNullOrEmpty(filename) && filename.Length > 20) {
                         var extension = filename.Split('.').Last();
-                        filename = filename.Substring (0, 10) + '.' + extension;
+                        filename = filename.Substring(0, 10) + '.' + extension;
                     }
 
                     string path = newFolderName + '\\' + filename;
 
-                    try
-                    {
-                        webClient.DownloadFile (new Uri (uriString), path);
+                    try {
+                        webClient.DownloadFile(new Uri(uriString), path);
                     }
-                    catch (Exception e)
-                    {
-                        _client.Log.Error (e.Source, e.Message);
+                    catch (Exception e) {
+                        _client.Log.Error(e.Source, e.Message);
                     }
-                    _client.Log.Info (this.ToString (), filename + " has been saved to: " + path);
+                    _client.Log.Info(this.ToString(), filename + " has been saved to: " + path);
                 }
             }
         }
 
-        private async Task GetPicAddressesFromChannel (Channel channel, ICollection<string> picUrLs, ulong? startFromThisMessageId, int numberOfPics)
-        {
-
+        private async Task GetPicAddressesFromChannel(Channel channel, ICollection<string> picUrLs,
+            ulong? startFromThisMessageId, int numberOfPics) {
             const int maxLimit = 100;
-            var messages = await channel.DownloadMessages(relativeMessageId: startFromThisMessageId, relativeDir: Relative.Before, limit: maxLimit);
+            var messages = await channel.DownloadMessages(relativeMessageId: startFromThisMessageId,
+                relativeDir: Relative.Before, limit: maxLimit);
 
-            foreach (var message in messages)
-            {
+            foreach (var message in messages) {
                 var messageText = message.Text;
                 var possiblePictureUrl = string.Empty;
 
-                if (!messageText.StartsWith ("http"))
-                    possiblePictureUrl = Regex.Match (messageText,
-                        @"((http|ftp|https):\/\/[\w\-_]+(\.[\w\-_]+)+([\w\-\.,@?^=%&amp;:/~\+#]*[\w\-\@?^=%&amp;/~\+#])?)").ToString ();
+                if (!messageText.StartsWith("http"))
+                    possiblePictureUrl = Regex.Match(messageText,
+                            @"((http|ftp|https):\/\/[\w\-_]+(\.[\w\-_]+)+([\w\-\.,@?^=%&amp;:/~\+#]*[\w\-\@?^=%&amp;/~\+#])?)")
+                        .ToString();
 
                 if (message.Attachments.Length > 0)
                     possiblePictureUrl = message.Attachments[0].Url;
@@ -81,18 +71,51 @@ namespace roboragi2
                 if (possiblePictureUrl.Length <= 0)
                     continue;
                 if (possiblePictureUrl.Length > 0 && picUrLs.Count < numberOfPics)
-                    picUrLs.Add (possiblePictureUrl);
+                    picUrLs.Add(possiblePictureUrl);
                 else
                     return;
             }
 
-            _client.Log.Info ("Number of pictures found: ", messages.Length.ToString ());
+            _client.Log.Info("Number of pictures found: ", messages.Length.ToString());
 
             if (messages.Length < maxLimit)
                 return;
-            else
-                await GetPicAddressesFromChannel (channel, picUrLs, messages.Last ().Id, numberOfPics);
+            await GetPicAddressesFromChannel(channel, picUrLs, messages.Last().Id, numberOfPics);
+        }
 
+        public async Task ArchiveUser(ulong userID, Channel channel) {
+            const int maxLimit = 100;
+
+            ulong? startFromThisMessageId = null;
+            var tempMessages = await channel.DownloadMessages(relativeMessageId: startFromThisMessageId,
+                relativeDir: Relative.Before, limit: maxLimit);
+            var userMessages = new List<Message>();
+
+            do {
+                foreach (var message in tempMessages) {
+                    if (message.User != null) {
+                        if (message.User.Id == userID) {
+                            userMessages.Add(message);
+                            _client.Log.Info(this.ToString(), "found a message:" + message.ToString());
+                        }
+                    }
+                    else {
+                        var messageString = message.ToString();
+                        // todo make it work (username as param for command)
+                    }
+                }
+
+                tempMessages = await channel.DownloadMessages(relativeMessageId: tempMessages.Last().Id,
+                    relativeDir: Relative.Before, limit: maxLimit);
+            } while (tempMessages.Length == maxLimit);
+
+            TextWriter tw = new StreamWriter(userID.ToString() + ".txt");
+
+            foreach (var userMessage in userMessages) {
+                tw.WriteLine(userMessage);
+            }
+
+            tw.Close();
         }
     }
 }
--- roboragi2/Commands.cs
+++ roboragi2/Commands.cs
@@ -10,56 +10,76 @@ namespace roboragi2
 {
     internal class Commands
     {
-        public Commands (ref DiscordClient client) {
+        public Commands(ref DiscordClient client)
+        {
             _client = client;
         }
 
-        private bool CheckIfUserHasRole (Channel channel, String roleName, User user) {
-            var roles = channel.Server.FindRoles (roleName);
-            foreach (var role in roles) {
+        private bool CheckIfUserHasRole(Channel channel, String roleName, User user)
+        {
+            var roles = channel.Server.FindRoles(roleName);
+            foreach (var role in roles)
+            {
                 if (role != null)
-                    return user.HasRole (role);
+                    return user.HasRole(role);
             }
             return false;
         }
 
         private DiscordClient _client;
 
-        public void CreateCommands () {
-            _client.UsingCommands (x => {
+        public void CreateCommands()
+        {
+            _client.UsingCommands(x =>
+            {
                 x.PrefixChar = '!';
                 x.HelpMode = HelpMode.Public;
             });
 
-            _client.GetService<CommandService> ().CreateGroup ("do", cgb => {
-                cgb.CreateCommand ("greet")
-                    .Alias (new string[] {"gr", "hi"})
-                    .Description ("Greets a person.")
-                    .Parameter ("GreetedPerson", ParameterType.Required)
-                    .Do (
-                        async e => {
-                            await e.Channel.SendMessage ($"{e.User.Name} greets {e.GetArg ("GreetedPerson")}");
+            _client.GetService<CommandService>().CreateGroup("do", cgb =>
+            {
+                cgb.CreateCommand("greet")
+                    .Alias(new string[] { "gr", "hi" })
+                    .Description("Greets a person.")
+                    .Parameter("GreetedPerson", ParameterType.Required)
+                    .Do(
+                        async e =>
+                        {
+                            await e.Channel.SendMessage($"{e.User.Name} greets {e.GetArg("GreetedPerson")}");
                         });
 
-                cgb.CreateCommand ("bye")
-                    .Alias (new string[] {"bb", "gb"})
-                    .Description ("Greets a person.")
-                    .Parameter ("GreetedPerson", ParameterType.Required)
-                    .Do (
-                        async e => {
-                            await e.Channel.SendMessage ($"{e.User.Name} says goodbye to {e.GetArg ("GreetedPerson")}");
+                cgb.CreateCommand("bye")
+                    .Alias(new string[] { "bb", "gb" })
+                    .Description("Greets a person.")
+                    .Parameter("GreetedPerson", ParameterType.Required)
+                    .Do(
+                        async e =>
+                        {
+                            await e.Channel.SendMessage($"{e.User.Name} says goodbye to {e.GetArg("GreetedPerson")}");
                         });
             });
 
-            _client.GetService<CommandService> ().CreateGroup ("archive", cgb => {
-                cgb.CreateCommand ("pics")
-                    .Description ("Saves pictures from channel.")
-                    .Parameter ("numberOfPics", ParameterType.Optional)
-                    .AddCheck (
-                        (command, user, channel) => CheckIfUserHasRole (channel, "King of the Weebs", user))
-                    .Do (async e => {
-                        var archiver = new Archiver (ref _client);
-                        await archiver.ArchivePics (e.Channel, int.Parse (e.GetArg ("numberOfPics")));
+            _client.GetService<CommandService>().CreateGroup("archive", cgb =>
+            {
+                cgb.CreateCommand("pics")
+                    .Description("Saves pictures from channel.")
+                    .Parameter("numberOfPics", ParameterType.Optional)
+                    .AddCheck(
+                        (command, user, channel) => CheckIfUserHasRole(channel, "King of the Weebs", user))
+                    .Do(async e =>
+                    {
+                        var archiver = new Archiver(ref _client);
+                        await archiver.ArchivePics(e.Channel, int.Parse(e.GetArg("numberOfPics")));
+                    });
+                cgb.CreateCommand("user")
+                    .Description("Saves user messages from server")
+                    .Parameter("userID", ParameterType.Required)
+                    .AddCheck(
+                        (command, user, channel) => CheckIfUserHasRole(channel, "King of the Weebs", user))
+                    .Do(async e =>
+                    {
+                        var archiver = new Archiver(ref _client);
+                        await archiver.ArchiveUser(ulong.Parse(e.GetArg("userID")), e.Channel);
                     });
             });
         }
